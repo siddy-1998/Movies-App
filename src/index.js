6b43545197d7e9a3f8da45d7d1a3f7fd7b32e592
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-07-29 13:24:57 +0530

    create store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('store',store);
+console.log('STATE', store.getState());
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
