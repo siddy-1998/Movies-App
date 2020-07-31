f6cd4c839f8c05dd13a06126a34530cc1a50bb8d
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-07-31 15:53:18 +0530

    add change tab functionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
-import { ADD_MOVIES , ADD_TO_FAVOURITES , REMOVE_FROM_FAVOURITES } from '../actions';
+import { ADD_MOVIES , ADD_TO_FAVOURITES , REMOVE_FROM_FAVOURITES , SET_SHOW_FAVOURITES} from '../actions';
 
 const initialMoviesState = {
     list : [],
-    favourites : []
+    favourites : [] ,
+    showFavourites : false
 }
 export default function movies(state = initialMoviesState,action){
     // if(action.type === ADD_MOVIES){
@@ -34,6 +35,12 @@ export default function movies(state = initialMoviesState,action){
             return{
                 ...state ,
                 favourites : filteredArray                
+            }
+
+        case SET_SHOW_FAVOURITES :
+            return{
+                ...state,
+                showFavourites : action.val
             }
 
         default :
