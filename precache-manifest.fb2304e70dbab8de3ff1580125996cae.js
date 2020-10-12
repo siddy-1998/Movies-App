55321bf915e40a6b0e621a2a4e0a534ee4d152e0
siddy1998 <siddhantrathoreinfinity@gmail.com>
2020-10-12 16:12:45 +0530

    Updates

---
+++ precache-manifest.fb2304e70dbab8de3ff1580125996cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c43165ff6bbb5e5ce380724a5210c8a",
+    "url": "/Movies-App/index.html"
+  },
+  {
+    "revision": "3f32f56853db9384bff5",
+    "url": "/Movies-App/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "bed9497a99e210b12df2",
+    "url": "/Movies-App/static/js/2.1cd8ed3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movies-App/static/js/2.1cd8ed3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f32f56853db9384bff5",
+    "url": "/Movies-App/static/js/main.e9099943.chunk.js"
+  },
+  {
+    "revision": "7c94baa6fdd69716b228",
+    "url": "/Movies-App/static/js/runtime-main.7c1e9a36.js"
+  }
+]);
